e89c417fd6ecaebbffa47144df7c223c7c650f0d
bradthx <bradthx@gmail.com>
2015-08-19 11:37:49 -0400

    initial commit

---
+++ gotime.sh
@@ -0,0 +1,2 @@
+while true; do /usr/bin/php /var/www/time/time.php > /dev/ttyUSB0; sleep .1; done
+
---
+++ time.php
@@ -0,0 +1,13 @@
+<?php
+
+date_default_timezone_set('UTC');
+
+//$now = date("h-i-y  z:H:s");
+
+$now = date("z H:i:s");
+echo "\n" . "   " . $now . " DTZ=00" . "\n";
+
+
+//echo $now . "\r\n";
+
+
